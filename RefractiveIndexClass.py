dce3bf9ac39d6ef7f6e73d931dda410ce84e03e0
Jack Morse <jack.morse2001@gmail.com>
2023-11-20 12:55:37 +0000

    General Work

--- RefractiveIndexClass.py
+++ RefractiveIndexClass.py
@@ -21,7 +21,7 @@ class RefractiveIndex:
     def _nm2um(nm):
         return nm / 1000
     
-    def n_fs(wavelength):
+    def n_fs(variable, parameter = "wavelength"):
         """
         refractive index of fused silica.
         
@@ -31,18 +31,30 @@ class RefractiveIndex:
 
         Parameters
         -------
-        wavelength [nm]
+        variable
+        parameter = "wavelength" [nm] or "omega" [rad Hz]
          
     
         Returns
         -------
         value for refractive index of fused silica at wavelength.
         """
-        wavelength = RefractiveIndex._nm2um(wavelength)
+        print("In the function")
+        print(parameter.lower())
+        if parameter.lower() not in ["wavelength", "omega"]:
+            raise NotImplementedError("Error: Input parameter needs to match 'wavelength' or 'omega'")
+        if parameter.lower() == "omega":
+            variable = 2 * np.pi * 3e17 / variable # Converts from omega to wavelength in nm, c is s.o.l in nm/s.
+            
+        wavelength = RefractiveIndex._nm2um(variable)
         return np.sqrt(1 + (0.6961663 * wavelength**2) / (wavelength**2  - 0.0684043**2) + (0.4079426 * wavelength**2) / (wavelength**2 - 0.1162414**2) + (0.8974794 * wavelength**2) / (wavelength**2 - 9.896161**2))
-    
+      
     def _deriv(f, wavelength, h=1e-6):
         return (1/(2  * h)) * (f(wavelength + h) - f(wavelength - h))
     
     def n_group(n, wavelength, h=1e-6):
         return n(wavelength) - wavelength * RefractiveIndex._deriv(n, wavelength, h)
+    
+# import matplotlib.pyplot as plt
+# import numpy as np
+# lambdas = np.linspace(1)
